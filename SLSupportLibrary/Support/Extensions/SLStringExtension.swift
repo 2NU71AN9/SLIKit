be7da7fc8cede2542fb4808766c38b145ee192a2
孙梁 <1145373054@qq.com>
2018-09-25 10:27:06 +0800

    update

--- SLSupportLibrary/Support/Extensions/SLStringExtension.swift
+++ SLSupportLibrary/Support/Extensions/SLStringExtension.swift
@@ -86,3 +86,50 @@ public extension String {
         return dateFormatter.date(from: self)
     }
 }
+
+extension String {
+    
+    /// 通过下标访问或赋值
+    subscript(start:Int, length:Int) -> String {
+        get {
+            let index1 = self.index(self.startIndex, offsetBy: start)
+            let index2 = self.index(index1, offsetBy: length)
+            return String(self[index1..<index2])
+        }
+        set {
+            let tmp = self
+            var s = ""
+            var e = ""
+            for (idx, item) in tmp.characters.enumerated() {
+                if (idx < start) {
+                    s += "\(item)"
+                }
+                if (idx >= start + length) {
+                    e += "\(item)"
+                }
+            }
+            self = s + newValue + e
+        }
+    }
+    
+    /// 通过下标访问或赋值
+    subscript(index:Int) -> String {
+        get {
+            return String(self[self.index(self.startIndex, offsetBy: index)])
+        }
+        set {
+            let tmp = self
+            self = ""
+//            tmp.enumerateSubstrings(in: tmp.startIndex ..< tmp.index(tmp.startIndex, offsetBy: tmp.count)) { (str, inx, inx2, stop) in
+//
+//            }
+            for (idx, item) in tmp.characters.enumerated() {
+                if idx == index {
+                    self += "\(newValue)"
+                } else {
+                    self += "\(item)"
+                }
+            }
+        }
+    }
+}
