d26e560dc11f1a7e3d4db5469f99dcac72b8db1d
2UN7 <1491859758@qq.com>
2020-12-30 15:49:53 +0800

    获取连接WiFi的相关信息

--- SLSupportLibrary/Support/Tools/SLTools.swift
+++ SLSupportLibrary/Support/Tools/SLTools.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import UIKit
+import SystemConfiguration.CaptiveNetwork
 
 public class SLTools {
     
@@ -52,5 +53,46 @@ public class SLTools {
         let urlString = "https://itunes.apple.com/cn/app/id" + appid + "?mt=12"
         let url = URL(string: urlString)
         UIApplication.shared.open(url!, options: [:], completionHandler: nil)
+    }
+    
+    /// 获取连接wifi的ip地址, 需要定位权限和添加Access WiFi information
+    public static func getWiFiIP() -> String? {
+        var address: String?
+        // get list of all interfaces on the local machine
+        var ifaddr: UnsafeMutablePointer<ifaddrs>? = nil
+        guard getifaddrs(&ifaddr) == 0,
+              let firstAddr = ifaddr else { return nil }
+        for ifptr in sequence(first: firstAddr, next: { $0.pointee.ifa_next }) {
+            let interface = ifptr.pointee
+            // Check for IPV4 or IPV6 interface
+            let addrFamily = interface.ifa_addr.pointee.sa_family
+            if addrFamily == UInt8(AF_INET) || addrFamily == UInt8(AF_INET6) {
+                // Check interface name
+                let name = String(cString: interface.ifa_name)
+                if name == "en0" {
+                    // Convert interface address to a human readable string
+                    var addr = interface.ifa_addr.pointee
+                    var hostName = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+                    getnameinfo(&addr, socklen_t(interface.ifa_addr.pointee.sa_len), &hostName, socklen_t(hostName.count), nil, socklen_t(0), NI_NUMERICHOST)
+                    address = String(cString: hostName)
+                }
+            }
+        }
+        freeifaddrs(ifaddr)
+        return address
+    }
+    
+    /// 获取连接wifi的名字和mac地址, 需要定位权限和添加Access WiFi information
+    public static func getWifiNameWithMac() -> (String?, String?) {
+        let interfaces:NSArray = CNCopySupportedInterfaces()!
+        var ssid: String?
+        var mac: String?
+        for sub in interfaces {
+            if let dict = CFBridgingRetain(CNCopyCurrentNetworkInfo(sub as! CFString)) {
+                ssid = dict["SSID"] as? String
+                mac = dict["BSSID"] as? String
+            }
+        }
+        return (ssid, mac)
     }
 }
