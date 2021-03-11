620cae9ccb8f2d2ecaffdd86ce21a8af47991bc2
2UN7 <1491859758@qq.com>
2021-03-11 14:41:41 +0800

    添加ex

---
+++ SLSupportLibrary/Support/Extensions/Ex_ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  Ex_ViewController.swift
+//  SLSupportLibrary
+//
+//  Created by 孙梁 on 2021/3/11.
+//  Copyright © 2021 SL. All rights reserved.
+//
+
+import UIKit
+
+public extension SLEx where Base: UIViewController {
+    @discardableResult
+    func push(_ animated: Bool) -> SLEx {
+        SL.visibleVC?.navigationController?.pushViewController(base, animated: animated)
+        return self
+    }
+    
+    @discardableResult
+    func pop(_ animated: Bool) -> SLEx {
+        base.navigationController?.popViewController(animated: animated)
+        return self
+    }
+    
+    @discardableResult
+    func present(_ animated: Bool, completion: (() -> Void)?) -> SLEx {
+        SL.visibleVC?.present(base, animated: animated, completion: completion)
+        return self
+    }
+    
+    @discardableResult
+    func dismiss(_ animated: Bool, completion: (() -> Void)?) -> SLEx {
+        base.dismiss(animated: animated, completion: completion)
+        return self
+    }
+}
