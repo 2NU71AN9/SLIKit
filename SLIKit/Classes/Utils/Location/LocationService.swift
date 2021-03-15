8aa3e56e5d5eb5e4bf4d48bee2376bcbc991464c
2UN7 <1491859758@qq.com>
2021-03-15 17:11:45 +0800

    update

--- SLIKit/Classes/Utils/Location/LocationService.swift
+++ SLIKit/Classes/Utils/Location/LocationService.swift
@@ -1,8 +1,8 @@
 //
 //  LocationService.swift
-//  FgIphone
+//  SLIKit
 //
-//  Created by Chu Guimin on 2018/5/24.
+//  Created by 孙梁 on 2018/5/24.
 //  Copyright © 2018年 fg. All rights reserved.
 //
 
@@ -51,11 +51,14 @@ public final class SLLocationService: NSObject {
         manager.requestWhenInUseAuthorization()
         manager.delegate = self
         manager.desiredAccuracy = kCLLocationAccuracyThreeKilometers
-        manager.desiredAccuracy = kCLLocationAccuracyBest // 精确的读取位置信息，消耗电量较多
-        manager.activityType = .fitness //  此设置会智能调节：例如当你过马路或者停止的时候，它会智能的更改配置帮用户节省电量
-        manager.distanceFilter = 10.0   // 重新定位的最小变化距离。不影响电量，位移信息不是一条直线而是有很多锯齿，高精度的 distanceFilter 可以减少锯齿，给你一个更精确地轨迹，但是，太高的精度值会让你的轨迹像素化(看到很多马赛克)，所以10m是一个相对比较合适的值
+        // 精确的读取位置信息，消耗电量较多
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        // 此设置会智能调节：例如当你过马路或者停止的时候，它会智能的更改配置帮用户节省电量
+        manager.activityType = .fitness
+        // 重新定位的最小变化距离(m)。不影响电量，位移信息不是一条直线而是有很多锯齿，高精度的 distanceFilter 可以减少锯齿，给你一个更精确地轨迹，但是，太高的精度值会让你的轨迹像素化(看到很多马赛克)，所以10m是一个相对比较合适的值
+        manager.distanceFilter = 10.0
 //        manager.requestAlwaysAuthorization()
-        //        manager.pausesLocationUpdatesAutomatically = false
+//        manager.pausesLocationUpdatesAutomatically = false
 //        manager.allowsBackgroundLocationUpdates = true
         return manager
     }()
@@ -111,4 +114,30 @@ extension SLLocationService: CLLocationManagerDelegate {
             }
         }
     }
+}
+
+
+public struct Location {
+    var location: CLLocation
+    var placemark: CLPlacemark?
+}
+
+extension Location {
+    /// 地址
+    var addressDesc: String? {
+        placemark?.locality ?? placemark?.name ?? placemark?.country
+    }
+    /// 经度
+    var longitude: Double {
+        location.coordinate.longitude
+    }
+    /// 纬度
+    var latitude: Double {
+        location.coordinate.latitude
+    }
+    
+//    var gaoDeLocation: CLLocation? {
+//        let coor = AMapCoordinateConvert(location.coordinate, .GPS)
+//        return CLLocation(latitude: coor.latitude, longitude: coor.longitude)
+//    }
 }
