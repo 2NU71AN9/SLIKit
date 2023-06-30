d7d5fac381fc559a4198bfedd90ccb15a10401c9
sunliang <1491859758@qq.com>
2023-06-30 16:53:55 +0800

    倒计时

--- SLIKit/Classes/Utils/SLTools.swift
+++ SLIKit/Classes/Utils/SLTools.swift
@@ -14,6 +14,10 @@ import SystemConfiguration.CaptiveNetwork
 import swiftScan
 #endif
 
+#if canImport(RxSwift)
+import RxSwift
+#endif
+
 public extension SL {
     static var tools: SLTools.Type {
         return SLTools.self
@@ -124,5 +128,28 @@ public class SLTools {
     public static func makeBarCode(content: String, size: CGSize) -> UIImage? {
         LBXScanWrapper.createCode128(codeString: content, size: size, qrColor: .black, bkColor: .white)
     }
+    #endif
+    
+    #if canImport(RxSwift)
+    /// 倒计时
+    /// - Parameters:
+    ///   - second: 倒计时的秒数
+    ///   - immediately: 是否立即开始，true 时将立即开始倒计时，false 时将在 1 秒之后开始倒计时
+    ///   - duration: 倒计时的过程
+    /// - Returns: 倒计时结束时的通知
+    public static func countdown(second: Int, immediately: Bool = true, duration: ((Int) -> Void)?) -> Single<Void> {
+        guard second > 0 else { return Single<Void>.just(()) }
+        if immediately { duration?(second) }
+        return Observable<Int>
+            .interval(RxTimeInterval.seconds(1), scheduler: MainScheduler.instance)
+            .map { second - (immediately ? ($0 + 1) : $0) }
+            .take(second + (immediately ? 0 : 1))
+            .do(onNext: { (index) in
+                duration?(index)
+            })
+            .filter { return $0 == 0 }
+            .map { _ in return () }
+            .asSingle()
+     }
     #endif
 }
